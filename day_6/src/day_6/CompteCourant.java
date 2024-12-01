955edd6a0bf6bfb4f7c922d85b66a793d101a80e
wahidridi <wahidridi@gmail.com>
2024-12-01 23:17:15 +0100

    compte bancaire interface et implementaion

--- day_6/src/day_6/CompteCourant.java
+++ day_6/src/day_6/CompteCourant.java
@@ -1,5 +1,39 @@
 package day_6;
 
-public class CompteCourant {
+public class CompteCourant implements CompteBancaire {
+	private String numCompte ;
+	private double solde;
+	private double rouge;
+	public String getNumCompte() {return numCompte;	}
+	public void setNumCompte(String numCompte) {this.numCompte = numCompte;	}
+	public double getSolde() {return solde;	}
+	public void setSolde(double solde) {this.solde = solde;	}
+	public double getRouge() {return rouge;	}
+	public void setRouge(double rouge) {this.rouge = rouge;	}
+	
+
+	public CompteCourant(String numCompte, double solde, double rouge) {
+		this.numCompte = numCompte;
+		this.solde = solde;
+		this.rouge = rouge;
+	}
+	public void afficher() {
+		System.out.println("num compte est : "+numCompte);
+		System.out.println("le solde est : "+solde);
+		System.out.println("rouge autorise est : "+rouge);		
+	}
+
+	public void debiter(double montant) {
+	if(solde +rouge > montant)
+	{solde -=montant;
+	System.out.println("le nouveau solde est : "+solde);
+	}
+		
+	}
+
+	public void crediter(double montant) {
+	solde+=montant;
+	System.out.println("le nouveau solde est : "+solde);		
+	}
 
 }
--- day_6/src/day_6/CompteEpargne.java
+++ day_6/src/day_6/CompteEpargne.java
@@ -1,5 +1,45 @@
 package day_6;
 
-public class CompteEpargne {
+public class CompteEpargne implements CompteBancaire{
+	private String numCompte ;
+	private double solde;
+	private double tauInteret;
+	public String getNumCompte() {return numCompte;	}
+	public void setNumCompte(String numCompte) {this.numCompte = numCompte;	}
+	public double getSolde() {return solde;	}
+	public void setSolde(double solde) {this.solde = solde;	}
+	public double getTauInteret() {return tauInteret;	}
+	public void setTauInteret(double tauInteret) {this.tauInteret = tauInteret;	}
+
+	public CompteEpargne(String numCompte, double solde, double tauInteret) {
+		this.numCompte = numCompte;
+		this.solde = solde;
+		this.tauInteret = tauInteret;
+	}
+	@Override
+	public void afficher() {
+		System.out.println("num compte est : "+numCompte);
+		System.out.println("le solde est : "+solde);
+		System.out.println("le taux d'interet est : "+tauInteret);			
+		
+	}
+
+	@Override
+	public void debiter(double montant) {
+		if(solde > montant)
+		{solde -=montant;
+		System.out.println("le nouveau solde est : "+solde);
+		}
+	}
+
+	@Override
+	public void crediter(double montant) {
+		solde+=montant;
+		System.out.println("le nouveau solde est : "+solde);		
+	}
+	public void CalculInteret() {
+		double interet = solde *tauInteret/100;
+		System.out.println("l'interet est : "+interet);
+	}
 
 }
